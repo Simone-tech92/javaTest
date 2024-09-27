0805044f725e6df9e0861ffbbfb1d1841952602a
Simone <Simone@DESKTOP-I2O45U8.home>
2024-09-27 15:47:04 +0200

    add.method1

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -4,7 +4,11 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
-
+		metodo1();
+	}
+	
+	private static void metodo1() {
+		System.out.println("metodo1");
 	}
 
 }
